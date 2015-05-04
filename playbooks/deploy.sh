124bc51b563305e8ba96f58d38071bd746024c17
Ubuntu <ubuntu@ip-10-93-162-176.ec2.internal>
2015-05-04 12:36:38 +0000

    Add predeploy extras to deploy.sh

--- playbooks/deploy.sh
+++ playbooks/deploy.sh
@@ -1,4 +1,7 @@
 export LC_ALL=C
 sudo locale-gen
-
-sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," -e edx_platform_repo=https://github.com/cyanna/edx-platform.git -e edx_platform_version='master'
+sudo apt-get install -y libffi-dev python-pip xmlsec1 python-dev build-essential 
+sudo -H pip install --upgrade pip 
+sudo -H pip install --upgrade virtualenv 
+sudo -H pip install --upgrade mercurial
+sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," -e edx_platform_repo=https://github.com/cyanna/edx-platform.git -e edx_platform_version='astea-30Apr'
